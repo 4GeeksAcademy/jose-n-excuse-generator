dae34e99cfae3c6b08d2aa6cc314bd51b852076b
jlnunezg10 <juniorng10@gmail.com>
2025-01-18 00:49:03 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+// // // // /* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let arrExcuses = [who, action, what, when];
+  let newExcuse = [];
+
+  for (let element of arrExcuses) {
+    newExcuse.push(element[Math.floor(Math.random() * element.length)]);
+  }
+
+  // console.log(newExcuse.join(" "));
+
+  document.getElementById("excuse").innerHTML = newExcuse.join(" ");
 };
